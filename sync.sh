fe445386a8a9ed1001a06748316a2e91fd52008a
kameshX007 <kameshbook@gmail.com>
2022-09-17 14:03:48 +0530

    #

--- sync.sh
+++ sync.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 log=rclone_google_kameshbook.log
 if [ -f "$log" ]; then  
-echo "$log exist, deleting it..." && rm $log && "Deleted old log"  
+echo "$log exist, deleting it..." && rm $log && echo "Deleted old log";  
 fi
 touch processing;
 rclone -v --log-file=rclone_google_kameshbook.log sync google_kameshbook:cloud/music/ /home/ubuntu/downloads/music/cloud/google_kameshbook
